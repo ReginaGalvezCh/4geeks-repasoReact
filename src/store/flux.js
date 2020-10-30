9720f1f6ccc7937dcdf8897a7064d7daf52d76da
Manuel Ortega Carcamo <mortega@apgca.cl>
2020-10-29 21:34:48 -0400

    fix: merge

--- src/store/flux.js
+++ src/store/flux.js
@@ -1,12 +1,29 @@
+// const urlAPI = 'https://3001-ba220300-3d3c-4573-83ff-47ec6566b625.ws-us02.gitpod.io';
+const urlAPI = 'http://localhost:3001';
+
 const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
             productList: [],
             brandList: [],
-            shopList: []
+            shopList: [],
+			featuredList: [],
 		},
 		actions: {
-
+			getFeatured: async () => {
+				const config = {
+					method: "GET",
+					headers: {
+						"Content-type": "application/json"
+					}
+				};
+                const response = await fetch(
+					`${urlAPI}/featured`,
+					config
+				);
+				const json = await response.json();
+				setStore({ featuredList: json });
+			},
             getProducts: async () => {
 				const config = {
 					method: "GET",
@@ -33,7 +50,7 @@ const getState = ({ getStore, setStore }) => {
 					}
 				};
 				const response = await fetch(
-					"http://localhost:3001/marcas",
+					`${urlAPI}/marcas`,
 					config
 				);
 				const json = await response.json();
--- src/view/Home.jsx
+++ src/view/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFemale, faHatCowboy, faChild } from '@fortawesome/free-solid-svg-icons'
 import Slider from '../components/Slider'
+import FeaturedList from '../components/FeaturedList'
 
 const Home = () =>{
 
@@ -19,6 +20,7 @@ return (
 
         <div className="col-lg-9">
             <Slider/>
+            <FeaturedList/>
         </div>
     </div>
 
