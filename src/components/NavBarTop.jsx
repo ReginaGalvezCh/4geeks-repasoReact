1db21500e35604644895e2791810bf057d371973
Andrea Parra <aparra001@gmail.com>
2020-10-31 14:56:48 -0300

    Agregue Link en Navbartop

--- src/components/NavBarTop.jsx
+++ src/components/NavBarTop.jsx
@@ -1,44 +1,54 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from "react-router-dom";
 import { Link } from 'react-router-dom';
-import {Navbar, Nav, Button } from 'react-bootstrap';
+import { Navbar, Nav, Button } from 'react-bootstrap';
 
-const NavBarTop = (props) =>{
+const NavBarTop = (props) => {
     const history = useHistory();
 
- 
 
-    const handleFormSubmit = async (e)=>{
+
+    const handleFormSubmit = async (e) => {
         e.preventDefault();
-   
+
     }
+    <Link to="/"> home </Link>
 
-
-    return(
+    return (
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
             <div class="container">
-            <a class="navbar-brand" href="#">Carro de compras</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ml-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Inicio
-                    <span class="sr-only">(current)</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Productos</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Tiendas</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Marcas</a>
-                </li>
-                </ul>
-            </div>
+                <Link to="/">
+                    <span className="navbar-brand">Carro de compras</span>
+                </Link>
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                            <Link to="/">
+                                <span class="sr-only">Inicio</span>
+                            </Link>
+                        </li>
+                        <li class="nav-item">
+                            <Link to="/products">
+                                <span className="nav-link" >Productos</span>
+                            </Link>
+
+                        </li>
+                        <li class="nav-item">
+                            <Link to="/sales">
+                                <span className="nav-link" >Tiendas</span>
+                            </Link>
+                        </li>
+                        <li class="nav-item">
+                            <Link to="/marcas">
+                                <span className="nav-link" >Marcas</span>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
 
